0247a36fcd769e20bc20990c1b8add3dfb3e37bd
Vishal Rakhecha <rakhecha.vishal18@gmail.com>
2024-02-14 01:16:46 +0530

    Connecting backend for data storage

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// firebase.js
+import firebase from 'firebase/app';        // Import the functions you need from the SDKs you need
+import 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARbXq8w6WCBLzX2Knm2-3K4jVStdBtAqI",
+  authDomain: "valentine-2fa04.firebaseapp.com",
+  projectId: "valentine-2fa04",
+  storageBucket: "valentine-2fa04.appspot.com",
+  messagingSenderId: "804124398638",
+  appId: "1:804124398638:web:6bc648a979c4594528fbbf"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firestore = firebaseApp.firestore();
+
+export { firestore };
